a20ea375bf8687d269968dccb0247a21299edf31
Nyanguk <lobgd9150@gmail.com>
2020-06-03 23:16:14 +0900

    중심좌표가 추출되었는지 확인하는 코드를 추가(각 좌표에 해당하는 점찍기)

--- Moment.py
+++ Moment.py
@@ -6,6 +6,8 @@ ccl = {}
 
 
 img_color = cv.imread('AutoSolding/PCB1.jpg')
+
+TestImg = img_color.copy()
 img_gray = cv.cvtColor(img_color, cv.COLOR_BGR2GRAY)
 ret, img_binary = cv.threshold(img_gray, 127, 255, 0)
 contours, hierarchy = cv.findContours(img_binary, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
@@ -17,23 +19,20 @@ for cnt in contours:
     if area> 50 and area<250:
         cv.drawContours(img_color, [cnt], 0, (255, 0, 0), 1)  # blue
 
-for cnt in contours:
-    mmt = cv.moments(cnt)
-    
-    for key,value in mmt.items():
-        try : 
-            cx = int(mmt['m10']/mmt['m00']) 
-            cy = int(mmt['m01']/mmt['m00'])
-        except ZeroDivisionError:
-            pass
-        ccl[cx] = cy
-    
-    for key,value in ccl.items():
-        print('%d :\t %d' %(key,value))
-        
-    
+        mmt = cv.moments(cnt)
+        for key,value in mmt.items():
+            try : 
+                cx = int(mmt['m10']/mmt['m00']) 
+                cy = int(mmt['m01']/mmt['m00'])
+            except ZeroDivisionError:
+                pass
+            ccl[cx] = cy
 
+        for key,value in ccl.items():
+            cv.line(TestImg,(key,value),(key,value),(0,0,255),5)
+
+            print('%d :\t %d' %(key,value))
 
 cv.imshow("result", img_color)
-
+cv.imshow("TestCenterLocation" ,TestImg)
 cv.waitKey(0)

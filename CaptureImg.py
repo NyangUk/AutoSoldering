f2c3fb94d8c21fdafc94e305d0c933b80226b402
Nyanguk <lobgd9150@gmail.com>
2020-06-09 09:27:28 +0900

    메소드로 만듬

--- CaptureImg.py
+++ CaptureImg.py
@@ -2,29 +2,24 @@ import cv2 as cv
 
 
 # 웹캠으로 사진찍기
+def CapturePCB():
+    Pcbimg = cv.VideoCapture(0)
 
-Pcbimg = cv.VideoCapture(0)
+    while(True):
+        ret,PcbColor = Pcbimg.read()
 
-while(True):
-    ret,PcbColor = Pcbimg.read()
+        if ret==False:
+            continue
+        PcbGray = cv.cvtColor(PcbColor,cv.IMREAD_COLOR)
+        cv.imshow("gray", PcbGray)
+        if cv.waitKey(1)&0xFF==32:
+            cv.imwrite('PCB.jpg', PcbGray)
+            break
 
-    if ret==False:
-        continue
+        if cv.waitKey(1)&0xFF==27:
+            break
+    Pcbimg.release()
+    
+CapturePCB()
 
-
-    PcbGray = cv.cvtColor(PcbColor,cv.IMREAD_COLOR)
-
-    cv.imshow("gray", PcbGray)
-
-    if cv.waitKey(1)&0xFF==32:
-        cv.imwrite('PCB.jpg', PcbGray)
-        break
-
-    if cv.waitKey(1)&0xFF==27:
-        break
-
-
-
-
-Pcbimg.release()
-cv.destroyAllWindows()
+# cv.destroyAllWindows()

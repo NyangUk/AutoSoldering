c0aa897a4abff6be9df64b9389478cc4b29783a7
Nyanguk <lobgd9150@gmail.com>
2020-06-09 17:32:09 +0900

    파이썬 3.6버전으로 변경 (컨투어함수 에러 해결)

--- AvailablePoint.py
+++ AvailablePoint.py
@@ -5,6 +5,7 @@ import numpy as np
 
 #contours center location dictionary
 ccl = {} 
+SelectedCcl = {}
 img_color = cv.imread('AutoSolding/PCB1.jpg')
 h,w,c =img_color.shape[:]
 
@@ -16,7 +17,7 @@ def FindHole(OriginalImg):
 
     ImgGray = cv.cvtColor(ImgColor, cv.COLOR_BGR2GRAY)
     ret, img_binary = cv.threshold(ImgGray, 127, 255, 0)
-    contours, hierarchy = cv.findContours(img_binary, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
+    _,contours, hierarchy= cv.findContours(img_binary, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
 
 
     for cnt in contours:
@@ -45,7 +46,30 @@ def FindHole(OriginalImg):
     cv.imwrite('availablepoint.jpg',ImgColor)
     cv.waitKey(0)
 
+def SoldingArea():
+    img =  cv.imread("AutoSolding/result.jpg",cv.IMREAD_GRAYSCALE)
+    zero = zeroImg.copy()
+    ret, img_binary = cv.threshold(img, 127, 255, 0)
+    _,contours, hierarchy = cv.findContours(img_binary, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
+    for cnt in contours:
+        mmt = cv.moments(cnt)
+        for key,value in mmt.items():
+            try : 
+                cx = int(mmt['m10']/mmt['m00']) 
+                cy = int(mmt['m01']/mmt['m00'])
+            except ZeroDivisionError:
+                pass
+            SelectedCcl[cx] = cy
+        for key,value in SelectedCcl.items():
+            if ccl.get(key):
+                cv.line(zero,(key,value),(key,value),(255,0,0),13) # 중심좌표그리기
+                # cv.circle(zeroImg,(key,value),7,(0,255,0),-1)
+    # zero = cv.cvtColor(zero, cv.COLOR_GRAY2RGB)
+    cv.imshow('final',zero)
+    cv.waitKey(0)
 
+                
 
 FindHole(img_color)
+SoldingArea()
 

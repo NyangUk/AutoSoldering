3bbe29e6407b8684f3e92ca047ee792d232a6057
Nyanguk <lobgd9150@gmail.com>
2020-06-11 23:58:29 +0900

    실제 이미지에서 쓰이기 위해 변수조정+저장 취소 기능추가

--- roi.py
+++ roi.py
@@ -2,12 +2,13 @@ import cv2
 import numpy as np 
 
 # 클릭 이벤트 선언
-events = [i for i in dir(cv2) if 'EVENT' in i] 
+ROIevents = [i for i in dir(cv2) if 'EVENT' in i] 
 
 # 클릭이 되었을때 드래그 할것 이므로 Click flag 넘기기
-OriginalImg = np.zeros((640,600,3),np.uint8)
-result1 = np.zeros((640,600,3),np.uint8)
-mask = np.zeros((640,600,3),np.uint8)
+OriginalImg = cv2.imread('PCB(0).jpg') 
+H,W,C =OriginalImg.shape[:]
+RoiImg = np.zeros((H,W,C),np.uint8)
+mask = np.zeros((H,W,C),np.uint8)
 
 # click = False     
 x1,y1,x2,y2 = -1,-1,-1,-1
@@ -15,8 +16,8 @@ x1,y1,x2,y2 = -1,-1,-1,-1
 
 
 def CallMouse(event ,x,y,flags,param):
-    global x1,y1,x2,y2,result1,mask#,click
-    result1 = OriginalImg
+    global x1,y1,x2,y2,RoiImg,mask#,click
+    RoiImg = OriginalImg
     if event == cv2.EVENT_LBUTTONDOWN:  # 마우스 누를때
         # click = True
         x1,y1 =x,y
@@ -24,24 +25,28 @@ def CallMouse(event ,x,y,flags,param):
     elif event == cv2.EVENT_LBUTTONUP: # 마우스 땔때
         # click =False
         x2,y2 =x,y
-        cv2.rectangle(result1,(x1,y1),(x2,y2),(255,255,255),-1)
-        
+        cv2.rectangle(RoiImg,(x1,y1),(x2,y2),(0,0,0),-1)
+    
+     
 
 def CropRoi():
+    global RoiImg
     while True:
-        # cv2.imshow('img',mask)   
-        # cv2.imshow('result1',result1) 
-
-        k = cv2.waitKey(1) & 0xFF  # 입력을 기다리기
         cv2.imshow('img',OriginalImg)
         cv2.namedWindow('img')
         cv2.setMouseCallback('img',CallMouse)
 
-        # elif k ==ord('f'): 
-        if k == 27:               
+        if cv2.waitKey(1)&0xFF == 27:
+            print("roi 선택을 그만두셨습니다.")               
             break
+        elif cv2.waitKey(1)&0xFF == ord('s'):
+            cv2.imwrite('AppliedROI.jpg',RoiImg)
+            print("roi 선택 완료")   
+            break
+        elif cv2.waitKey(1)&0xFF == ord('r'):
+            RoiImg = OriginalImg
+            cv2.imshow('img',OriginalImg)
+            print("roi 선택을 다시합니다.")       
 
-OriginalImg = cv2.imread("AutoSolding/PCB1.jpg")
-OriginalImg = cv2.resize(OriginalImg ,(640,600))
 CropRoi()
 
